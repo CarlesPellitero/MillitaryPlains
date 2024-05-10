d2ccc20a2b55be1b5f5e951c64a42101cb1c5ffa
Bits16 <86315718+RetroBits16@users.noreply.github.com>
2024-05-10 17:31:45 +0200

    expandir puesto

---
+++ JS/HideAndShow.js
@@ -0,0 +1,44 @@
+function fightersFunction() {
+  var x = document.getElementById("fighters-body");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function twinFunction() {
+  var x = document.getElementById("twin-body");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function strikeFunction() {
+  var x = document.getElementById("strike-body");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function bombersFunction() {
+  var x = document.getElementById("bombers-body");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function aircraftFunction() {
+  var x = document.getElementById("aircraft-body");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
