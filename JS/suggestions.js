a0d60990ea8b9e7f4862494eaa3a55d8c3407fe6
Carles Pelltiero <104790563+CarlesPellitero@users.noreply.github.com>
2024-05-12 13:33:10 +0200

    update encabezado, piedepagina

---
+++ JS/suggestions.js
@@ -0,0 +1,57 @@
+const searchContainer = document.querySelector('.search-input-box');
+const inputSearch = searchContainer.querySelector('input');
+const boxSuggestions = document.querySelector('.container-suggestions');
+
+const searchLink = document.querySelector('a');
+
+inputSearch.onkeyup = (e) =>{
+    let userData = e.target.value;
+    let emptyArray = [];
+
+    if (userData){
+        emptyArray = suggestions.filter(data => {
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        
+        emptyArray = emptyArray.map(data => {
+            return data = `<li>${data}</li>`
+        });
+        searchContainer.classList.add('active');
+        showSuggestions(emptyArray);
+
+        /* Busqueda */
+        let allList = boxSuggestions.querySelectorAll('li');
+
+        allList.forEach(li => {
+            li.setAttribute('onclick', 'select(this)')
+        });
+
+    } else {
+        searchContainer.classList.remove('active')
+    }
+};
+
+function select(element){
+
+    let selectUserData = element.textContent;
+    inputSearch.value = selectUserData;
+
+    /* Arreglar el href no lo concatena bien */
+    searchLink.href = 'Avion${inputSearch.value}.html';
+    searchContainer.classList.remove('active')
+
+}
+
+
+const showSuggestions = list => {
+    let listData
+
+    if(!list.length){
+        userValue = inputSearch.value;
+        listData = ` `;
+        /*listData = `<li>${userValue}</li>`;*/
+    } else{
+        listData = list.join(' ');
+    }
+    boxSuggestions.innerHTML = listData;
+};
